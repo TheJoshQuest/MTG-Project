8562d7398c7168515fcf794facd9b9672d0a0270
TheJoshQuest <118071373+TheJoshQuest@users.noreply.github.com>
2024-02-13 20:56:10 -0600

    modified:   game.py

--- game.py
+++ game.py
@@ -46,27 +46,30 @@ class MagicTheGathering():
     def next_step(self):
         if self.current_phase is None and self.current_step is None:
             self.start_step('Beginning Phase','Untap Step')
-        phase_order = ['Beginning Phase','Pre-combat Main Phase','Combat Phase','Post-combat Main Phase','Ending Phase']
-        beginning_phase_order = ['Untap Step','Upkeep Step','Draw Step']
-        main_phase_order = []
-        combat_phase_order = ['Beginning of Combat Step, Declare Attackers Step, Declare Blockers Step, Calculate Damage Step, End of Combat Step']
-        ending_phase_order = ['End Step', 'Cleanup Step']
-        phase_index = phase_order.index(self.current_phase)
-        if self.current_phase == 'Beginning Phase':
-            step_order = beginning_phase_order
-        if self.current_phase == 'Combat Phase':
-            step_order = combat_phase_order
-        if self.current_phase == 'Ending Phase':
-            step_order = ending_phase_order
-        if step_order is None:
-            step_order = main_phase_order
-        
-
-        next_phase = None
-        next_step = None
-        #step_index = phase_order
-        #phase_index = 
-        self.start_step(next_phase,next_step)
+        else:
+            phase_order = ['Beginning Phase','Pre-combat Main Phase','Combat Phase','Post-combat Main Phase','Ending Phase']
+            beginning_phase_order = ['Untap Step','Upkeep Step','Draw Step']
+            main_phase_order = []
+            combat_phase_order = ['Beginning of Combat Step, Declare Attackers Step, Declare Blockers Step, Calculate Damage Step, End of Combat Step']
+            ending_phase_order = ['End Step', 'Cleanup Step']
+            phase_dict = {'Beginning Phase':beginning_phase_order,
+                        'Pre-combat Main Phase':main_phase_order,
+                        'Combat Phase':combat_phase_order,
+                        'Post-combat Main Phase':main_phase_order,
+                        'Ending Phase':ending_phase_order}
+            phase_index = phase_order.index(self.current_phase)
+            step_order = phase_dict[self.current_phase]
+            step_index = step_order.index(self.current_step)
+            if step_index >= len(step_order)-1:
+                next_step_index = 0
+                next_phase_index = phase_index + 1
+            else:
+                next_step_index = step_index + 1
+                next_phase_index = phase_index
+            next_phase = phase_order[next_phase_index]
+            step_order = phase_dict[next_phase]
+            next_step = step_order[next_step_index]
+            self.start_step(next_phase,next_step)
 
     def start_step(self,phase = None, step = None):
                 
